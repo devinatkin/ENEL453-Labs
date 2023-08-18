9868e605151d44e958cb0027a00ecd9163862cb3
Devin <devinatkin@gmail.com>
2023-08-18 13:30:59 -0600

    create bitstream tcl created

--- LAB_1/SRC/create_bitstream.tcl
+++ LAB_1/SRC/create_bitstream.tcl
@@ -31,3 +31,13 @@ write_checkpoint -force post_route
 
 report_utilization -hierarchical -file utilization_hierarchical.rpt
 
+write_bitstream bitstream.bit
+
+open_hw_manager
+connect_hw_server -url 10.13.113.125:3121
+refresh_hw_server
+open_hw_target
+
+
+create_hw_bitstream -hw_device [lindex [get_hw_devices xc7a35t_0] 0] bitstream.bit
+program_hw_devices
