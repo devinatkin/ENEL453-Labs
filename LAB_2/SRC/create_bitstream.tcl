addac41ee1ac781467945200a5b19df4f8746944
Devin <devinatkin@gmail.com>
2023-09-11 17:04:12 -0600

    fixed create bitstream

--- LAB_2/SRC/create_bitstream.tcl
+++ LAB_2/SRC/create_bitstream.tcl
@@ -2,19 +2,10 @@
 create_project temp_project ./temp_project -part xc7a35tcpg236-1 -in_memory -force
 
 # Add source files (adjust paths and types as needed)
-read_verilog -sv ..\\SRC\\top_level.sv
-read_verilog -sv ..\\SRC\\timer.sv
-read_verilog -sv ..\\SRC\\time_counter.sv
-read_verilog -sv ..\\SRC\\stop_watch.sv
-read_verilog -sv ..\\SRC\\segment_mux.sv
-read_verilog -sv ..\\SRC\\one_second.sv
-read_verilog -sv ..\\SRC\\mode_mux.sv
-read_verilog -sv ..\\SRC\\display_driver.sv
-read_verilog -sv ..\\SRC\\bcd_binary.sv
-read_verilog -sv ..\\SRC\\for_students\\debounce.sv
-read_verilog -sv ..\\SRC\\for_students\\double_dabble.sv
+read_verilog -sv ..\\SRC\\top.sv
+read_verilog -sv ..\\SRC\\pwm_module.sv
 
-read_xdc ..\\SRC\\Basys3_Lab3_Provided_Constraints.xdc
+read_xdc ..\\SRC\\Basys3_Lab2_Provided_Constraints.xdc
 
 set_property top top [current_fileset]
 # Launch synthesis
@@ -43,7 +34,7 @@ report_utilization -hierarchical -file utilization_hierarchical.rpt
 write_bitstream bitstream.bit -force
 
 open_hw_manager
-connect_hw_server -url 10.13.141.4:3121
+connect_hw_server -url 10.13.117.25:3121
 refresh_hw_server
 open_hw_target
 
