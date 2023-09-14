e2da722ac559f781a1d89e011540f07b61b5b81e
Devin <devinatkin@gmail.com>
2023-09-14 09:19:24 -0600

    updated for additional calls

--- LAB_3/DISPLAY_SITE/segment_image.py
+++ LAB_3/DISPLAY_SITE/segment_image.py
@@ -1,7 +1,9 @@
-
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+import tempfile
+import os
+import uuid
 
 def recolor_segments(image, contours, colors):
     colored_image = image.copy()
@@ -12,61 +14,47 @@ def recolor_segments(image, contours, colors):
             cv2.drawContours(colored_image, [cnt], 0, (255, 255, 255), -1)
     return colored_image
 
-def generate_seven_segment_image_final(image, contours, bits):
+def find_contours(image):
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    _, thresholded = cv2.threshold(gray_image, 200, 255, cv2.THRESH_BINARY_INV)
+    contours, _ = cv2.findContours(thresholded, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    filtered_contours = [cnt for cnt in contours if 500 < cv2.contourArea(cnt) < 5000]
+    filtered_contours.sort(key=lambda cnt: cv2.boundingRect(cnt)[0] + cv2.boundingRect(cnt)[1]*image.shape[1])
+    return filtered_contours
+
+def generate_and_save_image(bits, image):
     if len(bits) != 7:
         raise ValueError("The bits input must be a list of 7 integers (0 or 1).")
-    print("Bits: ", bits)
-    corrected_bits = [bits[i] for i in [0,1,5,6,4,2,3]]
-    print("Corrected bits: ", corrected_bits)
+    
+    contours = find_contours(image)
+    
+    corrected_bits = [bits[i] for i in [0, 1, 5, 6, 4, 2, 3]]
     color_mapping = {0: 'white', 1: 'green'}
     segment_colors = [color_mapping[bit] for bit in corrected_bits]
+    
     generated_image = recolor_segments(image, contours, segment_colors)
-    return generated_image
+    
+    temp_dir = tempfile.gettempdir()
+    unique_filename = str(uuid.uuid4()) + "_seven_segment.png"
+    temp_file_path = os.path.join(temp_dir, unique_filename)
+    
+    cv2.imwrite(temp_file_path, generated_image)
+    
+    return generated_image, temp_file_path
 
-# Standard 7-segment encoding for numbers 0-9
-seven_segment_encoding = {
-    0: [1, 1, 1, 1, 1, 1, 0],
-    1: [0, 1, 1, 0, 0, 0, 0],
-    2: [1, 1, 0, 1, 1, 0, 1],
-    3: [1, 1, 1, 1, 0, 0, 1],
-    4: [0, 1, 1, 0, 0, 1, 1],
-    5: [1, 0, 1, 1, 0, 1, 1],
-    6: [1, 0, 1, 1, 1, 1, 1],
-    7: [1, 1, 1, 0, 0, 0, 0],
-    8: [1, 1, 1, 1, 1, 1, 1],
-    9: [1, 1, 1, 1, 0, 1, 1]
-}
+if __name__ == '__main__':
+    # Initialize your image here (you may use the existing code)
+    image_path = "SevenSegment.png"
+    image_np = cv2.imread(image_path)
 
-# Example usage
-# Load your image here
-image_path = "SevenSegment.png"
-image_np = cv2.imread(image_path)
+    # Example usage with bits corresponding to the number 1
+    bits = [0, 1, 1, 0, 0, 0, 0]
+    generated_image, temp_file_path = generate_and_save_image(bits, image_np)
 
-# Your contours should be determined here based on the image
-# Convert the image to grayscale
-gray_image = cv2.cvtColor(image_np, cv2.COLOR_BGR2GRAY)
+    # Display the image (Optional)
+    plt.imshow(cv2.cvtColor(generated_image, cv2.COLOR_BGR2RGB))
+    plt.axis('off')
+    plt.show()
 
-# Threshold the image
-_, thresholded = cv2.threshold(gray_image, 200, 255, cv2.THRESH_BINARY_INV)
-
-# Find contours
-contours, _ = cv2.findContours(thresholded, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-# Filter contours based on area to isolate the seven segments
-filtered_contours = [cnt for cnt in contours if 500 < cv2.contourArea(cnt) < 5000]  # Adjust these values as needed
-
-# Sort the contours from left-to-right, top-to-bottom
-filtered_contours.sort(key=lambda cnt: cv2.boundingRect(cnt)[0] + cv2.boundingRect(cnt)[1]*image_np.shape[1])
-
-contours = filtered_contours
-
-# Generate and display images for numbers 0-9
-fig, axes = plt.subplots(1, 10, figsize=(20, 2))
-for num, ax in zip(range(10), axes):
-    encoded_bits = seven_segment_encoding[num]
-    generated_image = generate_seven_segment_image_final(image_np, contours, encoded_bits)
-    ax.imshow(cv2.cvtColor(generated_image, cv2.COLOR_BGR2RGB))
-    ax.set_title(f"Number {num}")
-    ax.axis('off')
-
-plt.show()
+    # Print the temporary file path
+    print(f"Temporary file saved at: {temp_file_path}")
